41c573a48e509a019be67c625b05582814560b07
abrarShariar <shariarabrar@gmail.com>
2017-06-22 02:22:41 +0600

    category controller code updated

--- app/controllers/category.controller.ts
+++ app/controllers/category.controller.ts
@@ -41,6 +41,7 @@ export class CategoryController {
         sequelize.query("SELECT C1.id, C1.name, C1.parent,C1.image_list, C1.sub_category, C1.createdAt, C1.updatedAt, C2.name AS parent_name FROM Categories C1, Categories C2 WHERE C1.parent=C2.id ORDER BY C1.createdAt DESC")
             .spread((results, metadata) => {
                 result_data = results;
+                console.log(result_data);
                 async.each(results, function (data, callback) {
                     let category = data;
                     category["sub_category_list"] = [];
