69e0b10c0cebb26b0ff09d3a31c9cb1ee5f15079
abrarShariar <shariarabrar@gmail.com>
2017-09-05 09:40:28 +0600

    cart API updated

--- app/controllers/shopping-cart.controller.ts
+++ app/controllers/shopping-cart.controller.ts
@@ -9,7 +9,73 @@ export class ShoppingCartController {
 
     }
 
+
     static addToShoppingCart(req: Request, res: Response) {
+        let data = req.body;
+        model.ShoppingCart.findOrCreate({
+            where: {
+                userId: data["userId"]
+            }
+        }).then((result) => {
+            let temp = result[0];
+            let shoppingCartId = temp.dataValues.id;
+            //find the product in that shopping cart
+            model.CartItem.find({
+                where: {
+                    $and: [
+                        {
+                            shoppingCartId: shoppingCartId
+                        },
+                        {
+                            productId: data['productId']
+                        }
+                    ]
+                }
+            }).then((result) => {
+                if (!result) {
+                    //insert new product in a shopping cart
+                    model.CartItem.create({
+                        shoppingCartId: shoppingCartId,
+                        productId: data['productId'],
+                        quantity: data['quantity']
+                    }).then((item) => {
+                        res.send({data: item, status: true});
+                    }).catch((err) => {
+                        res.send({data: {}, status: false});
+                    });
+                } else {
+                    //update product quantity
+                    model.CartItem.update({
+                        quantity: data["quantity"]
+                    }, {
+                        where: {
+                            id: result["id"]
+                        }
+                    }).then((item) => {
+                        if (item) {
+                            res.send({status: true});
+                        } else {
+                            res.send({status: false});
+                        }
+                    }).catch((err) => {
+                        if (err) {
+                            res.send({status: false});
+                        }
+                    })
+                }
+            })
+
+        }).catch((err) => {
+            if (err) {
+                res.send({status: false});
+            } else {
+                res.send({status: true});
+            }
+        })
+    }
+
+
+    static addToShoppingCart_(req: Request, res: Response) {
         let data = req.body;
         model.ShoppingCart.findOrCreate({
             where: {
@@ -39,7 +105,9 @@ export class ShoppingCartController {
                     }).then((item) => {
                         res.send({data: item, status: true});
                     }).catch((err) => {
-                        res.send({data: {}, status: false});
+                        if(err){
+                            res.send({data: {}, status: false});
+                        }
                     });
                 } else {
                     //update product quantity
@@ -67,12 +135,11 @@ export class ShoppingCartController {
         })
     }
 
-
     //get the shopping cart of a particular user by userId
     static getShoppingCart(req: Request, res: Response) {
         let result_data = [];
         let userId = req.params.userId;
-        sequelize.query("SELECT ShoppingCarts.id, ShoppingCarts.userId, ShoppingCarts.createdAt, ShoppingCarts.updatedAt, CartItems.id, CartItems.productId, CartItems.quantity, CartItems.shoppingCartId FROM `ShoppingCarts`, `CartItems` WHERE (ShoppingCarts.id = CartItems.shoppingCartId AND ShoppingCarts.userId = "+ userId + ")")
+        sequelize.query("SELECT ShoppingCarts.id, ShoppingCarts.userId, ShoppingCarts.createdAt, ShoppingCarts.updatedAt, CartItems.id, CartItems.productId, CartItems.quantity, CartItems.shoppingCartId FROM `ShoppingCarts`, `CartItems` WHERE (ShoppingCarts.id = CartItems.shoppingCartId AND ShoppingCarts.userId = " + userId + ")")
             .spread((results, metadata) => {
                 result_data = results;
                 res.send({data: result_data, count: result_data.length, status: true});
